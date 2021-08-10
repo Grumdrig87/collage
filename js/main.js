77ce4de6fe6a2c4dc45f2b0274cd2cf47dd04af4
Grumdrig87 <70903272+Grumdrig87@users.noreply.github.com>
2021-08-10 17:48:58 +0300

    1

--- js/main.js
+++ js/main.js
@@ -113,5 +113,39 @@ if (jQuery('[data-eat]').length > 0) {
       }
     });
   };
+
+  var label = $("[data-input]").parent().find('label span').each(function() {$(this).text()});
+  var labelText = $("[data-text]").parent().find('label span').text();
+  console.log(labelText);
+  $("[data-input]").change(function (){
+    $(this).each(function(){
+      input = $(this).val();
+      var parent = $(this).parent().index();
+      if(input) {
+        $(this).parent().find('label').html(label[parent]);
+        $(this).parent().removeClass('alert');
+        $(this).parent().find('label').addClass('checked')
+      } else {
+        $(this).attr('placeholder','');
+        $('[data-text]').parent().find('label').html(labelText);
+        $(this).parent().find('label').removeClass('checked')
+      }
+    });
+  });
+  function requ(){
+    input = $(this).val();
+    if(!input) {
+      $(this).parent().addClass('alert');
+      $(this).parent().find('label').text('Ошибка заполнения');
+      $(this).attr('placeholder','Обязательное поле')
+    } else {
+      $(this).parent().removeClass('alert');
+      $(this).attr('placeholder','')
+    }
+  };
+  $("[data-subm]").click(function(){
+    $("[data-input]").each(requ);
+    $("[data-text]").each(requ);
+  });
   
 });
